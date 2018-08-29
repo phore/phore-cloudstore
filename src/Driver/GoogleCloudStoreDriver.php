8b05fba60a19a3d2f37c49b1fdd0ae4264cdb585
Matthias <m@tth.es>
2018-08-29 13:45:50 +0200

    bucket name

--- src/Driver/GoogleCloudStoreDriver.php
+++ src/Driver/GoogleCloudStoreDriver.php
@@ -29,7 +29,7 @@ class GoogleCloudStoreDriver implements CloudStoreDriver
             "keyFilePath" => $keyFilePath
         ]);
 
-        $this->bucket = $store->bucket("talpa-maschine-data");
+        $this->bucket = $store->bucket($bucketName);
     }
 
 
